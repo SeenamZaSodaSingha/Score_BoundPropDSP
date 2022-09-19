6b87617d2c9162540517628825119ea6df393e59
21 September <Thammanit.ch@gmail.com>
2022-09-19 19:48:12 +0700

    "First commit hw5_2"

---
+++ src/boundproperty/Listener.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boundproperty;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+/**
+ *
+ * @author SeenamZaSodaSingha
+ */
+public class Listener implements PropertyChangeListener {
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        System.out.println(evt.getNewValue());
+    }
+    
+}
